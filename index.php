78214a499ab4691ec9002f965ecd19a5310a5d30
chang180 <chang180@gmail.com>
2020-03-20 16:00:23 +0800

    operator

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 define("PI",3.14);
 echo PI."<br>";
 
-echo M_PI."<br>";
-echo pi()."<br>";
+echo M_PI."<br>"; //這是定值，比較快
+echo pi()."<br>"; //這是函數，慢一點點
 
 $a=1;
 $a=2.1;
@@ -12,5 +12,12 @@ print($a)."<br>";
 
 $studentInSchool=true;
 
+
+$c=-1;
+if($c) echo "true";
+else echo "false";
+echo "<br>";
+if(false==false) echo "true";
+else echo "false";
 
 ?>
