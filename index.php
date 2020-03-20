aa145779fbd0dd51d823c08cb7dd28eea1e91b67
chang180 <chang180@gmail.com>
2020-03-20 13:34:07 +0800

    add some new code

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-echo "Hello World!";
+echo "Hello World!"."<br> \n";
 $aa;
 $bb;
 $cc;
 $dd;
+
+// 做個九九乘法表玩玩
+for($i=2;$i<=9;$i++)
+{
+    for($j=2;$j<=9;$j++)
+    {
+        echo $i."X".$j."=".$j*$i."<br>\n";
+    }
+    echo "<br>\n";
+}
 ?>
 <!-- 終端機視窗： ctrl+` -->
 <h1>hello world</h1>
